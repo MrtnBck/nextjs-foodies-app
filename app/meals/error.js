4083f2839083713364c298f0010b15bf58c9c102
MrtnBck <martin.dev.dev@gmail.com>
2025-02-26 07:08:51 +0100

    handling error with error.js

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({}) {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to fetch a mail data, please try it again!</p>
+    </main>
+  );
+}
