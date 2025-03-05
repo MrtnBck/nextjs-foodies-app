5647d8cb601338df07613ea58a16699f182efef9
MrtnBck <martin.dev.dev@gmail.com>
2025-03-05 22:06:27 +0100

    form server side validation, form action, error display on the front end

--- app/meals/share/error.js
+++ app/meals/share/error.js
@@ -4,7 +4,7 @@ export default function Error({}) {
   return (
     <main className="error">
       <h1>An error occured!</h1>
-      <p>Failed to fetch a meal data. Please try it again!</p>
+      <p>Failed to create meal. Please try again later!</p>
     </main>
   );
 }
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -6,7 +6,11 @@ import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "../meals-form-submit";
 
+import { useActionState } from "react";
+
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,6 +44,7 @@ export default function ShareMealPage() {
             <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
